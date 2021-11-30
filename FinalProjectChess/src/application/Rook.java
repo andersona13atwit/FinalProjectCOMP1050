01338edb8966195b465342a7a1f817a572d55101
andersona13 <andersona13@PF2Z2EFY.wit.private>
2021-11-30 12:32:22 -0500

    Added all piece classes

---
+++ FinalProjectChess/src/application/Rook.java
@@ -0,0 +1,38 @@
+package application;
+
+public class Rook extends Piece {
+	public Tile location;
+	public Tile[] canMoveTo;
+	public boolean isSelected;
+
+	@Override
+	public int move() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public void update() {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void draw() {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	protected void toggleSelected() {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	protected void calcMoves() {
+		// TODO Auto-generated method stub
+
+	}
+
+}
