25fa5b37d9ce7a2827c961c1aea15bd67a9b24c6
merklem <90476830+merklem@users.noreply.github.com>
2021-12-11 01:20:35 -0500

    Update Knight.java

--- FinalProjectChess/src/application/Knight.java
+++ FinalProjectChess/src/application/Knight.java
@@ -4,7 +4,9 @@ import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 
 public class Knight extends Piece {
-	
+	int locationIndex;
+	int x2;
+	int y2;
 
 	
 	
@@ -26,8 +28,59 @@ public class Knight extends Piece {
 
 	@Override
 	protected void calcMoves() {
+		canMoveTo.clear();
+		locationIndex = Tile.tiles.indexOf(location);
+		x2 = locationIndex / 8;
+		y2 = locationIndex % 8;
 		// TODO Auto-generated method stub
-
+		//fr
+		if(x2 < 7 && y2 > 1) {
+			if((Tile.tiles2d[x2 + 1][y2 - 2].currentlyHeld == null) || Tile.tiles2d[x2 + 1][y2 - 2].currentlyHeld.isBlack != this.isBlack) {
+				canMoveTo.add(Tile.tiles2d[x2 + 1][y2 - 2]);
+			}
+		}
+		//rightup
+		if(x2 < 6 && y2 > 0) {
+			if((Tile.tiles2d[x2 + 2][y2 - 1].currentlyHeld == null) || Tile.tiles2d[x2 + 2][y2 - 1].currentlyHeld.isBlack != this.isBlack) {
+				canMoveTo.add(Tile.tiles2d[x2 + 2][y2 - 1]);
+			}
+		}
+		//left up
+		if(x2 > 1  && y2 > 0) {
+			if((Tile.tiles2d[x2 - 2][y2 - 1].currentlyHeld == null) || Tile.tiles2d[x2 - 2][y2 - 1].currentlyHeld.isBlack != this.isBlack) {
+				canMoveTo.add(Tile.tiles2d[x2 - 2][y2 - 1]);
+			}
+		}
+		//up left
+		if(x2 > 0 && y2 > 1) {
+			if((Tile.tiles2d[x2 - 1][y2 - 2].currentlyHeld == null) || Tile.tiles2d[x2 - 1][y2 - 2].currentlyHeld.isBlack != this.isBlack) {
+				canMoveTo.add(Tile.tiles2d[x2 - 1][y2 - 2]);
+			}
+		}
+		//down left
+		if(x2 > 0 && y2 < 6) {
+			if((Tile.tiles2d[x2 - 1][y2 + 2].currentlyHeld == null) || Tile.tiles2d[x2 - 1][y2 + 2].currentlyHeld.isBlack != this.isBlack) {
+				canMoveTo.add(Tile.tiles2d[x2 - 1][y2 + 2]);
+			}
+		}
+		//left down
+		if(x2 > 1 && y2 < 7) {
+			if((Tile.tiles2d[x2 - 2][y2 + 1].currentlyHeld == null) || Tile.tiles2d[x2 - 2][y2 + 1].currentlyHeld.isBlack != this.isBlack) {
+				canMoveTo.add(Tile.tiles2d[x2 - 2][y2 + 1]);
+			}
+		}
+		//right down
+		if(x2 < 6 && y2 < 7) {
+			if((Tile.tiles2d[x2 + 2][y2 + 1].currentlyHeld == null) || Tile.tiles2d[x2 + 2][y2 + 1].currentlyHeld.isBlack != this.isBlack) {
+				canMoveTo.add(Tile.tiles2d[x2 + 2][y2 + 1]);
+			}
+		}
+		//down right
+		if(x2 < 7 && y2 < 6) {
+			if((Tile.tiles2d[x2 + 1][y2 + 2].currentlyHeld == null) || Tile.tiles2d[x2 + 1][y2 + 2].currentlyHeld.isBlack != this.isBlack) {
+				canMoveTo.add(Tile.tiles2d[x2 + 1][y2 + 2]);
+			}
+		}
 	}
 
 }
