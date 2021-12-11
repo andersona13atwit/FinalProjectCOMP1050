9e0c7e571c46eb9ac27f3bcfdc040a89456efa94
merklem <merklem@PF2Z2S2C.wit.private>
2021-12-10 22:48:29 -0500

    rook bishop and queen movements updated

--- FinalProjectChess/src/application/Queen.java
+++ FinalProjectChess/src/application/Queen.java
@@ -5,8 +5,12 @@ import javafx.scene.image.ImageView;
 
 public class Queen extends Piece {
 	int temp;
+	int x;
+	int y;
+	int num;
 	Queen(Tile location, boolean isBlack) {
 		super(location, isBlack);
+		calcMoves();
 		if(isBlack) {
 			image = new Image("blackqueen.png");
 		}
@@ -23,14 +27,145 @@ public class Queen extends Piece {
 	protected void calcMoves() {
 		canMoveTo.clear();
 		temp = Tile.tiles.indexOf(location);
-		for(int i = 0; i < 64;i++) {
-			if ((i % 8 == temp % 8)& i != temp || i / 8 == temp / 8 & i != temp) {
-				if(Tile.tiles.get(i).currentlyHeld == null) {
-					canMoveTo.add(Tile.tiles.get(i));
-				}
+		x = temp / 8;
+		y = temp % 8;
+		
+		
+		
+		
+		//right
+		num = 7 - x;
+		for(int i = 1; i <= num; i++) {
+			if (Tile.tiles.get(temp + (i*8)).currentlyHeld == null) {
+				canMoveTo.add(Tile.tiles.get(temp +(i *8)));
+
+			}
+			else if(Tile.tiles.get(temp + (i*8)).currentlyHeld.isBlack != this.isBlack){
+				canMoveTo.add(Tile.tiles.get(temp +(i *8)));
+			}
+			if(Tile.tiles.get(temp + (i*8)).getPiece() != null ) {
+				break;
+			}
+		}
+		//left
+		num = x;
+		for(int i = 1; i <= num; i++) {
+			if (Tile.tiles.get(temp - (i*8)).currentlyHeld == null) {
+				canMoveTo.add(Tile.tiles.get(temp -(i *8)));
+
+			}
+			else if(Tile.tiles.get(temp - (i*8)).currentlyHeld.isBlack != this.isBlack){
+				canMoveTo.add(Tile.tiles.get(temp -(i *8)));
+			}
+			if(Tile.tiles.get(temp - (i*8)).getPiece() != null ) {
+				break;
+			}
+		}
+		//up
+		num = y;
+		for(int i = 1; i <= num; i++) {
+			if (Tile.tiles.get(temp - (i)).currentlyHeld == null) {
+				canMoveTo.add(Tile.tiles.get(temp -(i)));
+
+			}
+			else if(Tile.tiles.get(temp - (i)).currentlyHeld.isBlack != this.isBlack){
+				canMoveTo.add(Tile.tiles.get(temp -(i)));
+			}
+			if(Tile.tiles.get(temp - (i)).getPiece() != null ) {
+				break;
 			}
 		}
 		
+		//down
+		num = 7 - y;
+		for(int i = 1; i <= num; i++) {
+			if (Tile.tiles.get(temp + (i)).currentlyHeld == null) {
+				canMoveTo.add(Tile.tiles.get(temp +(i)));
+
+			}
+			else if(Tile.tiles.get(temp + (i)).currentlyHeld.isBlack != this.isBlack){
+				canMoveTo.add(Tile.tiles.get(temp +(i)));
+			}
+			if(Tile.tiles.get(temp + (i)).getPiece() != null ) {
+				break;
+			}
+		}
+		
+		
+		
+		num = y;
+		if(7 - x <= y)
+			num = 7 - x;
+		
+		
+		for(int i = 1; i <= num;i++) {
+			if (Tile.tiles.get(temp + (i*7)).currentlyHeld == null) {
+				canMoveTo.add(Tile.tiles.get(temp +(i *7)));
+
+			}
+			else if(Tile.tiles.get(temp + (i*7)).currentlyHeld.isBlack != this.isBlack){
+				canMoveTo.add(Tile.tiles.get(temp +(i *7)));
+			}
+			if(Tile.tiles.get(temp + (i*7)).getPiece() != null ) {
+				break;
+			}
+		}
+		//down left;
+		num = x;
+		if(7 - y <= x)
+			num = 7 - y;
+		
+		
+		for(int i = 1; i <= num;i++) {
+			if (Tile.tiles.get(temp - (i*7)).currentlyHeld == null) {
+				canMoveTo.add(Tile.tiles.get(temp -(i *7)));
+
+			}
+			else if(Tile.tiles.get(temp - (i*7)).currentlyHeld.isBlack != this.isBlack){
+				canMoveTo.add(Tile.tiles.get(temp -(i *7)));
+			}
+			if(Tile.tiles.get(temp - (i*7)).getPiece() != null ) {
+				break;
+			}
+		}
+		
+		//up left;
+		num = x;
+		if(y <= x)
+			num = y;
+		
+		
+		for(int i = 1; i <= num;i++) {
+			if (Tile.tiles.get(temp - (i*9)).currentlyHeld == null) {
+				canMoveTo.add(Tile.tiles.get(temp -(i *9)));
+
+			}
+			else if(Tile.tiles.get(temp - (i*9)).currentlyHeld.isBlack != this.isBlack){
+				canMoveTo.add(Tile.tiles.get(temp -(i *9)));
+			}
+			if(Tile.tiles.get(temp - (i*9)).getPiece() != null ) {
+				break;
+			}
+		}
+		
+		//down right;
+		num = 7 - x;
+		if(7 - y <= x)
+			num = 7 - y;
+		
+		
+		for(int i = 1; i <= num;i++) {
+			if ((temp + (i*9)) < 64 && Tile.tiles.get(temp + (i*9)).currentlyHeld == null) {
+				canMoveTo.add(Tile.tiles.get(temp +(i *9)));
+
+			}
+			else if((temp + (i*9)) < 64 && Tile.tiles.get(temp + (i*9)).currentlyHeld.isBlack != this.isBlack){
+				canMoveTo.add(Tile.tiles.get(temp +(i *9)));
+			}
+			if((temp + (i*9)) < 64 && Tile.tiles.get(temp + (i*9)).getPiece() != null ) {
+				break;
+			}
+		}
 		
 		// TODO Auto-generated method stub
 
