6680c8acb59566b739dfc683da38d2cc056e92b1
merklem <merklem@PF2Z2S2C.wit.private>
2021-12-10 23:53:48 -0500

    king

--- FinalProjectChess/src/application/King.java
+++ FinalProjectChess/src/application/King.java
@@ -5,7 +5,8 @@ import javafx.scene.image.ImageView;
 
 public class King extends Piece {
 	int temp;
-
+	int x2;
+	int y2;
 	King(Tile location, boolean isBlack) {
 		super(location, isBlack);
 		if(isBlack) {
@@ -25,20 +26,42 @@ public class King extends Piece {
 	@Override
 	protected void calcMoves() {
 		// TODO Auto-generated method stub
-//		canMoveTo.clear();
-//		temp = Tile.tiles.indexOf(location);
-//		x = temp / 8;
-//		y = temp % 8;
-//		
-//		if(x < 7) {
-//			canMoveTo.add(Tile.tiles.get(temp + 8));
-//		}
-//		if(x < 7 & y < 7) {
-//			canMoveTo.add(Tile.tiles.get(temp + 9));
-//		}
-//		if(x < 7 & y > 0) {
-//			canMoveTo.add(Tile.tiles.get(temp + 8));
-//		}
+		canMoveTo.clear();
+		temp = Tile.tiles.indexOf(location);
+		x2 = temp / 8;
+		y2 = temp % 8;
+		//right
+		if(x2 < 7 && (Tile.tiles.get(temp + 8).currentlyHeld == null || Tile.tiles.get(temp + 8).currentlyHeld.isBlack != this.isBlack)) {
+			canMoveTo.add(Tile.tiles.get(temp + 8));
+		}
+		//rd
+		if(x2 < 7 & y2 < 7 && (Tile.tiles.get(temp + 9).currentlyHeld == null || Tile.tiles.get(temp + 9).currentlyHeld.isBlack != this.isBlack)) {
+			canMoveTo.add(Tile.tiles.get(temp + 9));
+		}
+		//ru
+		if(x2 < 7 & y2 > 0 && (Tile.tiles.get(temp + 7).currentlyHeld == null || Tile.tiles.get(temp + 7).currentlyHeld.isBlack != this.isBlack)) {
+			canMoveTo.add(Tile.tiles.get(temp + 7));
+		}
+		//up
+		if(y2 > 0 && (Tile.tiles.get(temp - 1).currentlyHeld == null || Tile.tiles.get(temp - 1).currentlyHeld.isBlack != this.isBlack)) {
+			canMoveTo.add(Tile.tiles.get(temp - 1));
+		}
+		//lu
+		if(x2 > 0 & y2 > 0 && (Tile.tiles.get(temp - 9).currentlyHeld == null || Tile.tiles.get(temp - 9).currentlyHeld.isBlack != this.isBlack)) {
+			canMoveTo.add(Tile.tiles.get(temp - 9));
+		}
+		//l
+		if(x2 > 0 && (Tile.tiles.get(temp - 8).currentlyHeld == null || Tile.tiles.get(temp - 8).currentlyHeld.isBlack != this.isBlack)) {
+			canMoveTo.add(Tile.tiles.get(temp - 8));
+		}
+		///d
+		if(x2 > 0 & y2 < 7 && (Tile.tiles.get(temp - 7).currentlyHeld == null || Tile.tiles.get(temp - 7).currentlyHeld.isBlack != this.isBlack)) {
+			canMoveTo.add(Tile.tiles.get(temp - 7));
+		}
+		//down
+		if(y2 < 7 && (Tile.tiles.get(temp + 1).currentlyHeld == null || Tile.tiles.get(temp + 1).currentlyHeld.isBlack != this.isBlack)) {
+			canMoveTo.add(Tile.tiles.get(temp + 1));
+		}
 
 	}
 
