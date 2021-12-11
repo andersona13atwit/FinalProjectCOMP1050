a6f4ffdc4343594abeb583df038ff89120ccc1dd
merklem <merklem@PF2Z2S2C.wit.private>
2021-12-10 20:30:26 -0500

    updated queen and rook movement

--- FinalProjectChess/src/application/Queen.java
+++ FinalProjectChess/src/application/Queen.java
@@ -4,6 +4,7 @@ import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 
 public class Queen extends Piece {
+	int temp;
 	Queen(Tile location, boolean isBlack) {
 		super(location, isBlack);
 		if(isBlack) {
@@ -20,6 +21,16 @@ public class Queen extends Piece {
 
 	@Override
 	protected void calcMoves() {
+		canMoveTo.clear();
+		temp = Tile.tiles.indexOf(location);
+		for(int i = 0; i < 64;i++) {
+			if ((i % 8 == temp % 8)& i != temp) {
+				canMoveTo.add(Tile.tiles.get(i));
+			}
+		}
+		
+		
+		
 		// TODO Auto-generated method stub
 
 	}
--- FinalProjectChess/src/application/Rook.java
+++ FinalProjectChess/src/application/Rook.java
@@ -3,7 +3,9 @@ package application;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 public class Rook extends Piece {
+	int temp;
 	Rook(Tile location, boolean isBlack) {
+		
 		super(location, isBlack);
 		calcMoves();
 		if(isBlack) {
@@ -25,16 +27,30 @@ public class Rook extends Piece {
 	protected void calcMoves() {
 		// TODO Auto-generated method stub
 		canMoveTo.clear();
-		for(int i = 0; i < Tile.tiles.size(); i+=8) {
-			if(Tile.tiles.indexOf(location)+i<Tile.tiles.size() && Tile.tiles.get(Tile.tiles.indexOf(location)+i).currentlyHeld == null) {
-				canMoveTo.add(Tile.tiles.get(Tile.tiles.indexOf(location)+i));
+		temp = Tile.tiles.indexOf(location);
+		for(int i = 0; i < 64;i++) {
+			if ((i % 8 == temp % 8)& i != temp || i / 8 == temp / 8 & i != temp) {
+				if(Tile.tiles.get(i).currentlyHeld == null) {
+					canMoveTo.add(Tile.tiles.get(i));
+				}
 			}
 		}
-		for(int i = Tile.tiles.size()-1; i > 0; i-=8) {
-			if(Tile.tiles.indexOf(location)-i>0 ) {
-				canMoveTo.add(Tile.tiles.get(Tile.tiles.indexOf(location)-i));
-			}
-		}
+			
+		
+		
+		
+		
+		
+//		for(int i = 0; i < Tile.tiles.size(); i+=8) {
+//			if(Tile.tiles.indexOf(location)+i<Tile.tiles.size() && Tile.tiles.get(Tile.tiles.indexOf(location)+i).currentlyHeld == null) {
+//				canMoveTo.add(Tile.tiles.get(Tile.tiles.indexOf(location)+i));
+//			}
+//		}
+//		for(int i = Tile.tiles.size()-1; i > 0; i-=8) {
+//			if(Tile.tiles.indexOf(location)-i>0 ) {
+//				canMoveTo.add(Tile.tiles.get(Tile.tiles.indexOf(location)-i));
+//			}
+//		}
 	}
 
 }
